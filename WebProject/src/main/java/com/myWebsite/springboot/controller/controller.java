e847d56e08e4243ba93bb782ebe0d6aa2425b046
Timothy A <albantimothy@gmail.com>
2023-06-26 21:02:05 -0400

    finally finished the setup for the spring web

---
+++ WebProject/src/main/java/com/myWebsite/springboot/controller/controller.java
@@ -0,0 +1,14 @@
+package com.myWebsite.springboot.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class controller {
+    @GetMapping("/")
+    public String index() {
+        System.out.println("===================================Here==================================");
+        return "index";
+    }
+}
---
+++ WebProject/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@
+%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>View Books</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
